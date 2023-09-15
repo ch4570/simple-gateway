e74790f7cc015a92624299c5e8b6c742c4574afa
DevSeoRex <ckdekrn88@gmail.com>
2023-09-16 00:19:28 +0900

    Refactor : LoggingFilter DB 저장 로직 추가 중 #1

--- gateway-example/src/main/java/com/example/gatewayexample/filter/LoggingFilter.java
+++ gateway-example/src/main/java/com/example/gatewayexample/filter/LoggingFilter.java
@@ -1,5 +1,6 @@
 package com.example.gatewayexample.filter;
 
+import com.example.gatewayexample.domain.dto.RequestLogDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -20,7 +21,30 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
 
         return ((exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
             log.info(makeLog(exchange.getRequest()));
+            RequestLogDto logDto = createLogDto(exchange.getRequest());
+            // TODO : Kafka Topic Send -> 요청 정보 DB 저장
         })));
+    }
+
+    private String getHeaderByHeaderName(String headerName, ServerHttpRequest request) {
+        return request.getHeaders().get(headerName).get(0);
+    }
+
+    private RequestLogDto createLogDto(ServerHttpRequest request) {
+        String requestPath = request.getPath().toString();
+        String requestMethod = request.getMethod().toString();
+        String requestClientAddress = request.getRemoteAddress().getHostString();
+        String contentType = getHeaderByHeaderName("Content-Type", request);
+        String userAgent = getHeaderByHeaderName("User-Agent", request);
+
+
+        return RequestLogDto.builder()
+                .requestPath(requestPath)
+                .requestMethod(requestMethod)
+                .requestClientAddress(requestClientAddress)
+                .contentType(contentType)
+                .userAgent(userAgent)
+                .build();
     }
 
     static class Config {}
